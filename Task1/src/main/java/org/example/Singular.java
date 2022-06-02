6f4204b0238b9ede85917a0d7402ada3e147eefb
thuggeelya <ilya-fdmdh@mail.ru>
2022-06-02 18:16:33 +0700

    set to list #task1

--- Task1/src/main/java/org/example/Singular.java
+++ Task1/src/main/java/org/example/Singular.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashSet;
 
@@ -8,7 +9,7 @@ public class Singular {
     public static Collection<?> getNoDuplicatesCollection(Collection<?> collection) {
         return (collection == null) || (collection.isEmpty())
                 ? collection
-                : new LinkedHashSet<>(collection);
-//                : new ArrayList<>(new LinkedHashSet<>(collection));
+//                : new LinkedHashSet<>(collection);
+                : new ArrayList<>(new LinkedHashSet<>(collection));
     }
 }
