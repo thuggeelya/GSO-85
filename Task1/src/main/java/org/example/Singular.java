6c13e75926ebefba258fb38d66799029317c9794
thuggeelya <ilya-fdmdh@mail.ru>
2022-06-03 11:00:07 +0700

    review #task1

--- Task1/src/main/java/org/example/Singular.java
+++ Task1/src/main/java/org/example/Singular.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashSet;
 
@@ -9,7 +8,7 @@ public class Singular {
     public static Collection<?> removeDuplicates(Collection<?> collection) {
         return (collection == null) || (collection.isEmpty())
                 ? collection
-//                : new LinkedHashSet<>(collection);
-                : new ArrayList<>(new LinkedHashSet<>(collection));
+                : new LinkedHashSet<>(collection);
+//                : new ArrayList<>(new LinkedHashSet<>(collection));
     }
 }
--- Task1/src/test/java/org/example/SingularTest.java
+++ Task1/src/test/java/org/example/SingularTest.java
@@ -2,17 +2,14 @@ package org.example;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import static org.example.Singular.removeDuplicates;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 public class SingularTest {
 
-    static final List<String> INPUT_STRING_LIST = new ArrayList<String>(){{
+    static final List<String> INPUT_STRING_LIST = new ArrayList<String>() {{
         add("root");
         add("root");
         add("http");
@@ -22,21 +19,14 @@ public class SingularTest {
         add("root");
     }};
 
-    static final List<String> RESULT_STRING_LIST_ORDER_SAVED = new ArrayList<String>(){{
+    static final Set<String> RESULT_STRING_SET = new LinkedHashSet<String>() {{
         add("root");
         add("http");
         add("ftp");
         add("https");
     }};
 
-    static final List<String> RESULT_STRING_LIST_ORDER_CHANGED = new ArrayList<String>(){{
-        add("ftp");
-        add("https");
-        add("root");
-        add("http");
-    }};
-
-    static final List<Object> INPUT_OBJECT_LIST = new ArrayList<Object>(){{
+    static final List<Object> INPUT_OBJECT_LIST = new ArrayList<Object>() {{
         add(new Date(1000));
         add(new Date(2000));
         add(new Date(2000));
@@ -46,7 +36,7 @@ public class SingularTest {
         add("Greece");
     }};
 
-    static final List<Object> RESULT_OBJECT_LIST_ORDER_SAVED = new ArrayList<Object>(){{
+    static final Set<Object> RESULT_OBJECT_SET = new LinkedHashSet<Object>() {{
         add(new Date(1000));
         add(new Date(2000));
         add(12);
@@ -56,8 +46,7 @@ public class SingularTest {
 
     @Test
     public void makeNoDuplicatesSavingOrder() {
-        assertEquals("String collection mutation failed", removeDuplicates(INPUT_STRING_LIST), RESULT_STRING_LIST_ORDER_SAVED);
-        assertEquals("Object collection mutation failed", removeDuplicates(INPUT_OBJECT_LIST), RESULT_OBJECT_LIST_ORDER_SAVED);
-        assertNotEquals(removeDuplicates(INPUT_STRING_LIST), RESULT_STRING_LIST_ORDER_CHANGED);
+        assertEquals("String collection mutation failed", removeDuplicates(INPUT_STRING_LIST), RESULT_STRING_SET);
+        assertEquals("Object collection mutation failed", removeDuplicates(INPUT_OBJECT_LIST), RESULT_OBJECT_SET);
     }
 }
